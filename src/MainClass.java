74321fbacb61a5113abe6684faf9cead7ef9ed50
Mo-83 <m.barati83@hotmail.com>
2025-02-20 18:01:21 +0000

    Validation part has created

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
+
 import java.util.Scanner;
 
-=======
->>>>>>> origin/master
-public class MainClass {
-
+public class MainClass{
     public static void main(String[] args) {
-
-<<<<<<< HEAD
         int atmnumber = 12345;
         int atmpin = 123;
 
