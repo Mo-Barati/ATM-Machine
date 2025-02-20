3cc0e3c736c5d8aa343f9de164e9976141b5fa17
Mo-83 <m.barati83@hotmail.com>
2025-02-20 18:50:45 +0000

    MainClass updated

--- src/MainClass.java
+++ src/MainClass.java
@@ -14,10 +14,25 @@ public class MainClass{
         if((atmnumber == atmNumber) && (atmpin == atmpin)) {
             while (true){
                 System.out.println("1. View Available Balance \n2. Withdraw Balance \n3. Deposit\n4. View Statement \n5. Exit");
+                System.out.println("Enter your choice: ");
+                int choice = input.nextInt();
+                switch(choice) {
+                    case 1:
+
+                    case 2:
+
+                    case 3:
+
+                    case 4:
+
+                    case 5:
+                        System.out.println("Collect your ATM Card \n Thank you for using ATM Machine.");
+                        System.exit(0);
+                }
             }
         }
         else {
-            System.out.println("You have entered the different atm number");
+            System.out.println("Please enter a valid number");
             System.exit(0);
         }
     }
