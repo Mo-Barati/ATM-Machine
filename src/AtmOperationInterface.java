8deee6bd0e30dcfa004e63b0fb1d636daddfdc91
Mo-83 <m.barati83@hotmail.com>
2025-02-20 18:56:55 +0000

    AtmOperationInterface Ceated

---
+++ src/AtmOperationInterface.java
@@ -0,0 +1,8 @@
+public interface AtmOperationInterface {
+
+    public void viewBalance();
+    public void withdrawMoney(double amount);
+    public void depositMoney(double amount);
+    public void viewStatement();
+
+}
